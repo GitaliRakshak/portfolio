313844c1cbdb64ffa1d3e6068d643d8f3e523862
GitaliRakshak <132275624+GitaliRakshak@users.noreply.github.com>
2023-05-01 19:11:36 +0530

    Portfolio

---
+++ build/precache-manifest.f421c4479ee1ea29d404ccce2169044a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b902355f0998dfed69d91b6adad837c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e307ab4a9b0aa7ad06f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "7e307ab4a9b0aa7ad06f",
+    "url": "/static/js/main.9c1cc542.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
